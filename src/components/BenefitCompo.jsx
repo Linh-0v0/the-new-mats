2d047d8f6d83e42edf18259fa4a9f49b71553bcc
unknown <minhkhang09122003@gmail.com>
2022-11-24 15:24:50 +0700

    Add pictures succesfully

--- src/components/BenefitCompo.jsx
+++ src/components/BenefitCompo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-const stylePara = "font-size: ${(props) => props.theme.fontxxs};line-height: normal";
 
 const BenefitCompo = ({benefit,detail,svgSrc}) => {
     return (
         <div>
-            <obj data={svgSrc} width="2vw" hieght="2vh"></obj>
+            <img src={svgSrc} width="50vw" height="50vh" alt="" />
             <h3>{benefit}</h3>
             <p>{detail}</p>
         </div>
--- src/sections/Benefits.jsx
+++ src/sections/Benefits.jsx
@@ -12,18 +12,21 @@ const Section = styled.section`
 
 const Container = styled.div`
     display: grid; 
-    grid-template-columns: 25vw 25vw 25vw 25vw;
+    grid-template-columns: auto auto auto auto;
     grid-template-rows: 50vh 50vh;
-    text-align: center;
+    margin: 1vw;
     h1 {
         font-family: "Josefin Sans"
-        font-size: ${(props) => props.theme.font5xl};
+        font-size: ${(props) => props.theme.font6xl};
         font-weight: 800;
-        text-align: left;
-        color: ${(props) => `rgba(${props.theme.black}, 0.2)`};
+        color: ${(props) => `rgba(${props.theme.black}, 1)`};
         grid-column: 1/3;
-        grid-row:1/3;
+        grid-row: 1/3;
     }    
+    div {
+        margin: 1vw;
+        
+    }
 `;
 
 const Benefits = () => {
@@ -31,33 +34,32 @@ const Benefits = () => {
         <Section data-scroll-section>
             <Container>
                 <h1>Benefits of working with us</h1>
-                
                 <BenefitCompo 
                     benefit="Aesthetical"
                     detail="What we can bring to our customer possibly the profound 
                     and creativity in every products which will fill in the need and 
                     requirements of our guests"
-                    svgSrc="../icons/GoodLooking.svg"
+                    svgSrc="/icons/GoodLooking.svg"
                 />
                 <BenefitCompo
                     benefit="Technologies"
                     detail="All the essentials that we use and implement will be the 
                     top notch technologies nowaday to ensure that our and your visitor 
                     will experience the best performance in the product"
-                    svgSrc="../icons/Technology.svg"
+                    svgSrc="/icons/Technology.svg"
                 />
                 <BenefitCompo 
                     benefit="In time and precise"
                     detail="We are highly on schedule with everything and without doubt 
                     your time will be as valueable as ours"
-                    svgSrc="../icons/Time.svg"
+                    svgSrc="/icons/Time.svg"
                 />
                 <BenefitCompo
                     benefit="Well trained team"
                     detail="With strict filtering and selection our team form with skillful 
                     and professional members who will definitely assure that pleasant of our 
                     customer will always be prioritized"
-                    svgSrc="../icons/Teamwork.svg"
+                    svgSrc="/icons/Teamwork.svg"
                 />
             </Container>
         </Section>
