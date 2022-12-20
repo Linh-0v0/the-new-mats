9222c2ced7c6759848e19ec878943ce374f27767
Linh-0v0 <s3864120@rmit.edu.vn>
2022-12-21 00:08:43 +0700

    Add skybox

--- src/experience/World/Island/Island.jsx
+++ src/experience/World/Island/Island.jsx
@@ -1,9 +1,11 @@
 import IslandCollider from "./IslandCollider";
 import IslandMap from "./IslandMap";
+import Skybox from "./Skybox";
 
 export default class Island {
   constructor() {
     this.islandCollider = new IslandCollider();
+    this.skyBox = new Skybox();
     this.islandMap = new IslandMap();
   }
 
---
+++ src/experience/World/Island/Skybox.jsx
@@ -0,0 +1,27 @@
+import * as THREE from "three";
+import Experience from "../../Experience";
+
+export default class Skybox {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+
+    this.setModel()
+  }
+
+  setModel() {
+    const loader = new THREE.CubeTextureLoader();
+    //order: pos-x, neg-x, pos-y, neg-y, pos-z, neg-z
+    const texture = loader.load([
+        '/textures/top-sky.png',
+        '/textures/top-sky.png',
+        '/textures/uttertop-sky.jpeg',
+        '/textures/top-sky.png',
+        '/textures/top-sky.png',
+        '/textures/top-sky.png'
+    ]);
+    this.scene.background = texture;
+  }
+
+}
