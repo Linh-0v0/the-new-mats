c51211144e44a596259d77d0cbc72e4c77839f09
Linh-0v0 <s3864120@rmit.edu.vn>
2022-12-25 04:13:52 +0700

    add alert

--- src/pages/GameMap.jsx
+++ src/pages/GameMap.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useEffect } from "react";
 import Transitions from "../components/Transitions";
 
 const GameMap = () => {
+  useEffect(() => {
+    if (sessionStorage.getItem("loadingState") == "true") {
+      alert("Please wait a little bit for model to load 😘");
+    }
+  }, []);
   return (
     <Transitions>
-      <div ref={loadComp}></div>
       <div className="experience">
         <canvas className="experience-canvas"></canvas>
       </div>
